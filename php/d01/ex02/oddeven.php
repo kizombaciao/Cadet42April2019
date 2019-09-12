842ed9d89ce05e2a23c260ed78916177a270112a
Ching Chao <cchao@e1z2r4p31.42.us.org>
2019-09-11 18:16:48 -0700

    d

---
+++ php/d01/ex02/oddeven.php
@@ -0,0 +1,36 @@
+#!/usr/bin/php
+<?php
+    while (1)
+    {
+        echo "Enter a number:  ";
+        $line = trim(fgets(STDIN));
+        //if (!$line)
+        //    break;
+
+        // ??? why need below test
+        if (feof(STDIN))
+        {
+            echo "\n";
+            exit();
+        }
+        if (is_numeric($line))
+        {
+            if ($line % 2 == 0)
+                echo "The number ".$line." is even.\n";
+            else
+                echo "The number ".$line." is odd.\n";
+        }
+        else
+            echo "'".$line."'"." is not a number.\n";
+    }
+/*
+The feof() function checks 
+if the "end-of-file" (EOF) has been reached for an open file.
+https://www.w3schools.com/php/func_filesystem_feof.asp
+*/
+/*
+trim() removes white spaces and '\n' 
+from both sides of the strings 
+https://www.w3schools.com/php/func_string_trim.asp  
+*/
+?>
